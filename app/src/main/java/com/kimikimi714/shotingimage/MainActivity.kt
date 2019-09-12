a7842da8cb6ed2f8c7949e1a55543579f9088ec6
kimikimi714 <kimikimi714@gmail.com>
2019-09-13 00:07:18 +0900

    :+1: ちょっと改善
    - 座標を変数化
    - 最終座標をもっと下にしてみた
    - 色の指定をcolor.xmlからとってみた
    - 可視性修飾子をprivateに変更 ref: https://dogwood008.github.io/kotlin-web-site-ja/docs/reference/visibility-modifiers.html

--- app/src/main/java/com/kimikimi714/shotingimage/MainActivity.kt
+++ app/src/main/java/com/kimikimi714/shotingimage/MainActivity.kt
@@ -2,7 +2,6 @@ package com.kimikimi714.shotingimage
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
 import android.view.View
@@ -15,11 +14,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // viewの設定
         val testView = TestView(this)
         setContentView(testView)
 
         // 最終position
-        val endPosition = 1000
+        val endPosition = 2000
         val testAnimation = TestAnimation(testView, endPosition)
         // アニメーションの起動期間を設定
         testAnimation.setDuration(5000)
@@ -27,15 +27,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     inner class TestView(context: Context) : View(context) {
-        internal var paint = Paint()
+        private var paint = Paint()
+        private var leftAbsPos = 400f
+        private var topAbsPos = 100f
+        private var recWidth = 200f
 
         val position: Int
             get() = yval
 
         override fun onDraw(canvas: Canvas) {
-            paint.setColor(Color.argb(255, 125, 125, 125))
-            // (left, top, right, bottom) 左上(400, 100)を起点に幅200の矩形
-            canvas.drawRect(400f, (100 + yval).toFloat(), 600f, (300 + yval).toFloat(), paint)
+            // 矩形の色を設定
+            paint.setColor(resources.getColor(R.color.colorAccent))
+            // (left, top, right, bottom) 左上(400, 100)を起点に幅200の矩形を書いてyvalずつ下に向かって移動
+            canvas.drawRect(leftAbsPos, topAbsPos + yval, leftAbsPos + recWidth, topAbsPos + recWidth + yval, paint)
         }
 
         fun setPositon(pos: Int) {
