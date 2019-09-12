62ec43050acf5464328738e47d00b50a85bc4eb2
kimikimi714 <kimikimi714@gmail.com>
2019-09-12 23:36:16 +0900

    Revert ":+1: 矩形型を生成"
    This reverts commit 191eadd35c19da344d37d96b70a8642c1c2c4983.

--- app/src/main/java/com/kimikimi714/shotingimage/MainActivity.kt
+++ app/src/main/java/com/kimikimi714/shotingimage/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
-import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -32,10 +31,6 @@ class MainActivity : AppCompatActivity() {
             ShapeDrawable(OvalShape()).apply {
                 // If the color isn't set, the shape uses black as the default.
                 paint.color = 0xff74AC23.toInt()
-                // If the bounds aren't set, the shape can't be drawn.
-                setBounds(x, y, x + width, y + height)
-            }
-            ShapeDrawable(RectShape()).apply {
                 // If the bounds aren't set, the shape can't be drawn.
                 setBounds(x, y, x + width, y + height)
             }
