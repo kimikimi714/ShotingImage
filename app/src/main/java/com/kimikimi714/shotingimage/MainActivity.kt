4a01f56987c94224f8c9b0b94b514a32327640d9
kimikimi714 <kimikimi714@gmail.com>
2019-09-14 22:30:29 +0900

    :shower: remove unused package

--- app/src/main/java/com/kimikimi714/shotingimage/MainActivity.kt
+++ app/src/main/java/com/kimikimi714/shotingimage/MainActivity.kt
@@ -2,7 +2,6 @@ package com.kimikimi714.shotingimage
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.kimikimi714.shotingimage.animation.VerticalAnimation
 import com.kimikimi714.shotingimage.view.ShotingView
 
 class MainActivity : AppCompatActivity() {
