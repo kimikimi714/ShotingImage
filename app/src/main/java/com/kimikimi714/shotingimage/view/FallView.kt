7126faf136209cd9c4048b1d50e6286ccd261ac3
kimikimi714 <kimikimi714@gmail.com>
2019-09-14 12:36:18 +0900

    :+1: 最初の位置がランダムになるように修正

--- app/src/main/java/com/kimikimi714/shotingimage/view/FallView.kt
+++ app/src/main/java/com/kimikimi714/shotingimage/view/FallView.kt
@@ -5,34 +5,39 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
 import android.view.View
+import androidx.core.content.ContextCompat
 
 import com.kimikimi714.shotingimage.R
+import kotlin.random.Random
 
 class FallView(context: Context) : View(context) {
-    private var paint = Paint()
-
-    private var leftAbsPos = 400f
-    private var topAbsPos = 100f
-    private var recWidth = 200f
-
-    var yval = 0
+    private var yval = 0
     val position: Int
         get() = yval
 
     override fun onDraw(canvas: Canvas) {
-        // 矩形の色を設定
-        paint.color = resources.getColor(R.color.colorAccent)
-        // (left, top, right, bottom) 左上(400, 100)を起点に幅200の矩形を書いてyvalずつ下に向かって移動
-        val rect = RectF(
-            leftAbsPos,
-            topAbsPos + yval,
-            leftAbsPos + recWidth,
-            topAbsPos + recWidth + yval
-        )
-        canvas.drawRect(rect, paint)
+        val left = Random.nextFloat() * 800
+        val top = Random.nextFloat() * 500
+        canvas.drawRect(createRecf(left, top), accentPaint())
     }
 
     fun setPosition(pos: Int) {
         yval = pos
+    }
+
+    private fun accentPaint(): Paint {
+        val accent = Paint()
+        accent.color = ContextCompat.getColor(context, R.color.colorAccent)
+        return accent
+    }
+
+    private fun createRecf(left: Float, top: Float): RectF {
+        val recWidth = 200f
+        return RectF(
+            left,
+            top + yval,
+            left + recWidth,
+            top + recWidth + yval
+        )
     }
 }
