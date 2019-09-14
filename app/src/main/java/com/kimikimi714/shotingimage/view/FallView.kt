958ca08639652b92ee8cd83a31bada8f740e5d16
kimikimi714 <kimikimi714@gmail.com>
2019-09-14 12:57:16 +0900

    :bug: 描画のたびにランダムに位置が変わるのを修正
    一度位置が決まったら最後の地点に落ちるまで固定

--- app/src/main/java/com/kimikimi714/shotingimage/view/FallView.kt
+++ app/src/main/java/com/kimikimi714/shotingimage/view/FallView.kt
@@ -11,26 +11,47 @@ import com.kimikimi714.shotingimage.R
 import kotlin.random.Random
 
 class FallView(context: Context) : View(context) {
+    private var left = 0f
+    private var top = 0f
     private var yval = 0
     val position: Int
         get() = yval
 
+    /**
+     * 描画・再描画時に呼ばれる
+     */
     override fun onDraw(canvas: Canvas) {
-        val left = Random.nextFloat() * 800
-        val top = Random.nextFloat() * 500
         canvas.drawRect(createRecf(left, top), accentPaint())
     }
 
+    /**
+     * ビューのウインドウへの割当時によばれる
+     */
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        left = Random.nextFloat() * 800
+        top = Random.nextFloat() * 500
+    }
+
+    /**
+     * 高さを設定する
+     */
     fun setPosition(pos: Int) {
         yval = pos
     }
 
+    /**
+     * アクセントカラーで塗りつぶす
+     */
     private fun accentPaint(): Paint {
         val accent = Paint()
         accent.color = ContextCompat.getColor(context, R.color.colorAccent)
         return accent
     }
 
+    /**
+     * 矩形を作る
+     */
     private fun createRecf(left: Float, top: Float): RectF {
         val recWidth = 200f
         return RectF(
